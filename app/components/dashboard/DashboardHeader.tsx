007fe9c1f022d3862c378e97dabce2ce2e7ebbc1
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-10-20 15:35:22 +0700

    menyesuikan header dahsboard

--- app/components/dashboard/DashboardHeader.tsx
+++ app/components/dashboard/DashboardHeader.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-type Props = {
-  userName: string;
-};
+import { useEffect, useState } from "react";
 
-export default function DashboardHeader({ userName }: Props) {
+export default function DashboardHeader() {
+  const [userName, setUserName] = useState("User");
+
+  useEffect(() => {
+    const name = localStorage.getItem("name");
+    if (name) setUserName(name);
+  }, []);
+
   return (
     <header className="mb-8">
       <h1 className="text-3xl font-bold text-gray-800">
